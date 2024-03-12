09fcf2f47b7bf67b1002dc0b76519af1b549930a
Mohammed Nabeel <128857044+mohamadnabel@users.noreply.github.com>
2024-03-13 02:37:34 +0300

    add test 3 for last prosecc for checkout

--- src/magntoSoftwareTest/magntoTestTask.java
+++ src/magntoSoftwareTest/magntoTestTask.java
@@ -1,5 +1,6 @@
 package magntoSoftwareTest;
 
+import java.time.Duration;
 import java.util.List;
 import java.util.Random;
 
@@ -12,6 +13,8 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+import net.bytebuddy.build.Plugin.Factory.UsingReflection.Priority;
+
 public class magntoTestTask {
 	WebDriver driver = new ChromeDriver();
 	String URL = "https://magento.softwaretestingboard.com/";
@@ -21,14 +24,16 @@ public class magntoTestTask {
 	public void mySetup() {
 
 		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(9));// even if the problem solve the implicitly
+																			// stop waiting
 	}
 
-	@Test(invocationCount = 10)
+	@Test(invocationCount = 1, priority = 1)
 	public void addOneRandomItem() throws InterruptedException {
 
 		driver.get(URL);
 
-		int randomIndex = rand.nextInt(6);
+		int randomIndex = rand.nextInt(4);
 
 		WebElement contenier = driver.findElement(By.className("product-items"));
 
@@ -38,9 +43,8 @@ public class magntoTestTask {
 		listItems.get(randomIndex).click();
 		Thread.sleep(1500);
 
-		WebElement addRandomlyItem = driver.findElement(By.id("product-addtocart-button"));
-
 		if (driver.getCurrentUrl().contains("fusion") || driver.getCurrentUrl().contains("push")) {
+			WebElement addRandomlyItem = driver.findElement(By.name("product-addtocart-button"));
 
 			addRandomlyItem.click();
 		} else {
@@ -56,7 +60,10 @@ public class magntoTestTask {
 			List<WebElement> listRandomColors = colorsContainer.findElements(By.tagName("div"));
 			int randomColors = rand.nextInt(listRandomColors.size());
 			listRandomColors.get(randomColors).click();
+			WebElement addRandomlyItem = driver.findElement(By.id("product-addtocart-button"));
+
 			addRandomlyItem.click();
+			Thread.sleep(3000);
 
 		}
 
@@ -64,4 +71,43 @@ public class magntoTestTask {
 		;
 	}
 
+	@Test(priority = 2, description = "this is check out test")
+	public void checkOut() throws InterruptedException {
+
+//		System.out.println("Test num 2 it's done ");
+		String URLcheckOut = "https://magento.softwaretestingboard.com/checkout/cart/";
+		driver.get(URLcheckOut);
+		WebElement buttonToCheckOut = driver.findElement(By.xpath("//button[@data-role='proceed-to-checkout']"));
+		buttonToCheckOut.click();
+		Thread.sleep(4000);
+	};
+
+	@Test(priority = 3)
+	public void signupProcess() throws InterruptedException {
+
+		WebElement email = driver.findElement(By.id("customer-email"));
+		WebElement firstName = driver.findElement(By.name("firstname"));
+		WebElement lastName = driver.findElement(By.name("lastname"));
+		WebElement streetAddress = driver.findElement(By.name("street[0]"));
+		WebElement city = driver.findElement(By.name("city"));
+		WebElement state = driver.findElement(By.name("region_id"));
+		WebElement zipPostalCode = driver.findElement(By.name("postcode"));
+		WebElement country = driver.findElement(By.name("country_id"));
+		WebElement phoneNumber = driver.findElement(By.name("telephone"));
+
+		email.sendKeys("mohammadnabeel@gmail.com");
+		firstName.sendKeys("mohammad");
+		lastName.sendKeys("hussein");
+		streetAddress.sendKeys("marka");
+		city.sendKeys("amman");
+		state.sendKeys("jordan");
+		zipPostalCode.sendKeys("001548");
+		country.sendKeys("jordan");
+		phoneNumber.sendKeys("07914253688");
+
+		Thread.sleep(3000);
+
+		driver.findElement(By.cssSelector("button.action.continue.primary")).click();
+
+	}
 }
